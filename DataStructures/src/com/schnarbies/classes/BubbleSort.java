a94cce4046c89420e78210974d4d57745cc89ee8
dylan kessler <dylnksslr53@gmail.com>
2018-09-26 21:19:44 -0400

    array stuff

---
+++ DataStructures/src/com/schnarbies/classes/BubbleSort.java
@@ -0,0 +1,22 @@
+package com.schnarbies.classes;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArray = new int[7];
+		intArray[0] = 20;
+		intArray[1] = 35;
+		intArray[2] = -15;
+		intArray[3] = 7;
+		intArray[4] = 55;
+		intArray[5] = 1;
+		intArray[6] = -22;
+		
+		// unsorted partition index = 6 - this is the last index of the unsorted partition
+		// i = 0 - index used to traverse the array from left to right
+		
+		
+	}
+
+}
--- DataStructures/src/com/schnarbies/classes/Test.java
+++ DataStructures/src/com/schnarbies/classes/Test.java
@@ -2,9 +2,32 @@ package com.schnarbies.classes;
 
 public class Test {
 
+	// lecture 12
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] intArray = new int[7];
+		intArray[0] = 20;
+		intArray[1] = 35;
+		intArray[2] = -15;
+		intArray[3] = 7;
+		intArray[4] = 55;
+		intArray[5] = 1;
+		intArray[6] = -22;
+		
+		for(int i = 0; i < intArray.length; i++) {
+		//	System.out.println("index " + i + " = " + intArray[i]);
+		}
+		
+		// someone says "go find me the value 7 from the array
+		// O(n) search because we do not know what the index is
+		int index = -1;
+	    for (int i = 0; i < intArray.length; i++) {
+	        if (intArray[i] == 7) {
+	            index = i;
+	            break;
+            }
+        }
 
+        System.out.println("index = " + index);
 	}
 
 }
