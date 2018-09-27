bc1684c0017880235b1fc66b197bcce2bb4105ff
dylan kessler <dylnksslr53@gmail.com>
2018-09-27 19:21:21 -0400

    added document

--- DataStructures/src/com/schnarbies/classes/BubbleSort.java
+++ DataStructures/src/com/schnarbies/classes/BubbleSort.java
@@ -4,19 +4,33 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] intArray = new int[7];
-		intArray[0] = 20;
-		intArray[1] = 35;
-		intArray[2] = -15;
-		intArray[3] = 7;
-		intArray[4] = 55;
-		intArray[5] = 1;
-		intArray[6] = -22;
+		int[] intArray = {20,35,-15,7,55,1,-22};
 		
 		// unsorted partition index = 6 - this is the last index of the unsorted partition
 		// i = 0 - index used to traverse the array from left to right
+		// O(n^2) 
+		// each time we traverse through the array, we migrate the highest number to the end
+		// of the array
 		
-		
+		for(int lastUnsortedIndex = intArray.length-1; lastUnsortedIndex>0; lastUnsortedIndex--) {
+			for(int currentIndex = 0; currentIndex < lastUnsortedIndex; currentIndex++) {
+				if(intArray[currentIndex] > intArray[currentIndex+1]) {
+					swap(intArray, currentIndex, lastUnsortedIndex);
+				}
+			}
+		}
+		for(int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+	}
+
+	private static void swap(int[] intArray, int currentIndex, int lastUnsortedIndex) {
+		// TODO Auto-generated method stub
+		if(currentIndex == lastUnsortedIndex)
+			return;
+		int temp = intArray[lastUnsortedIndex];
+		intArray[lastUnsortedIndex] = intArray[currentIndex];
+		intArray[currentIndex] = temp;
 	}
 
 }
